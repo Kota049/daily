f615766ac7bec22b1f0494b87b6fc91899cb08e4
Kota <yuzukh0211@gmail.com>
2023-06-10 23:33:08 +0900

    jsxが動くように修正

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,8 +1,8 @@
 const App = () => {
-    return (
-        <>
-            <h1>hello world</h1>
-        </>
-    )
-}
-export default App
+  return (
+    <>
+      <h1>hello world</h1>
+    </>
+  );
+};
+export default App;

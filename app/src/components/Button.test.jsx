9722daec977c04e0c45dc3d6bb774888249421e9
Kota <yuzukh0211@gmail.com>
2023-06-11 17:13:37 +0900

    red set arg function to onclick

--- app/src/components/Button.test.jsx
+++ app/src/components/Button.test.jsx
@@ -1,6 +1,7 @@
-import { screen, render } from "@testing-library/react";
+import { screen, render, act } from "@testing-library/react";
 import Button from "./Button";
 import getButton from "../tests/utils/getButton";
+import userEvent from "@testing-library/user-event";
 
 describe("Button", () => {
   it("has button element", () => {
@@ -10,5 +11,14 @@ describe("Button", () => {
   it("show arg text in button", () => {
     render(<Button message="message" />);
     expect(getButton("message")).not.toBeNull();
+  });
+  it("set arg function to onclick", () => {
+    const action = jest.fn();
+    render(<Button message="message" action={action} />);
+    const button = getButton("message");
+    act(() => {
+      userEvent.click(button);
+    });
+    expect(action).toHaveBeenCalled();
   });
 });

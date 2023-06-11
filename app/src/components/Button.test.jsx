cd60af3ac64871937c49a9946a8a8f85cde960d0
Kota <yuzukh0211@gmail.com>
2023-06-11 17:00:06 +0900

    red show arg text in button

--- app/src/components/Button.test.jsx
+++ app/src/components/Button.test.jsx
@@ -1,12 +1,14 @@
 import { screen, render } from "@testing-library/react";
 import Button from "./Button";
-it("suit", () => {
-  expect(1).toBe(1);
-});
+import { findRenderedComponentWithType } from "react-dom/test-utils";
 
 describe("Button", () => {
   it("has button element", () => {
     render(<Button />);
     expect(screen.getByText("button", { selector: "button" })).not.toBeNull();
+  });
+  it("show arg text in button", () => {
+    render(<Button message="message" />);
+    expect(screen.getByText("message", { selector: "button" })).not.toBeNull();
   });
 });

4370469b98d0cc89adcb92092dc2a4d2f2987a8f
Kota <yuzukh0211@gmail.com>
2023-06-11 19:07:41 +0900

    return tweet as state

--- app/src/hooks/useKindOfTweet.test.jsx
+++ app/src/hooks/useKindOfTweet.test.jsx
@@ -4,9 +4,13 @@ it("suit", () => {
   expect(1).toBe(1);
 });
 describe("useKindOfTweet", () => {
-  it("return tweet and function", () => {
+  it("return string and function", () => {
     const [state, updater] = useKindOfTweet();
     expect(typeof state).toBe("string");
     expect(typeof updater).toBe("function");
+  });
+  it("return tweet as state", () => {
+    const [state] = useKindOfTweet();
+    expect(state).toBe("tweet");
   });
 });

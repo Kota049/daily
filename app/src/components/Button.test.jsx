bdf3d39eee8179f37032230d39b6ef6b36a9850b
Kota <yuzukh0211@gmail.com>
2023-06-11 16:56:45 +0900

    red has button element

--- app/src/components/Button.test.jsx
+++ app/src/components/Button.test.jsx
@@ -1,3 +1,12 @@
+import { screen, render } from "@testing-library/react";
+import Button from "./Button";
 it("suit", () => {
   expect(1).toBe(1);
+});
+
+describe("Button", () => {
+  it("has button element", () => {
+    render(<Button />);
+    expect(screen.getByText("button", { selector: "button" })).not.toBeNull();
+  });
 });

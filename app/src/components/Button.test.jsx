ebe1f7fc91cf93860ba593c89a553084c9d11a47
Kota <yuzukh0211@gmail.com>
2023-06-11 18:27:14 +0900

    refactor delete duplicate ,delete definition used once

--- app/src/components/Button.test.jsx
+++ app/src/components/Button.test.jsx
@@ -4,19 +4,19 @@ import getButton from "../tests/utils/getButton";
 import userEvent from "@testing-library/user-event";
 
 describe("Button", () => {
+  const message = "message";
   it("has button element", () => {
     render(<Button />);
     expect(getButton("button")).not.toBeNull();
   });
   it("show arg text in button", () => {
-    render(<Button message="message" />);
-    expect(getButton("message")).not.toBeNull();
+    render(<Button message={message} />);
+    expect(getButton(message)).not.toBeNull();
   });
   it("set arg function to onclick", async () => {
     const fn = jest.fn();
-    render(<Button message="message" hoge={fn} />);
-    const button = getButton("message");
-    await userEvent.click(button);
+    render(<Button message={message} hoge={fn} />);
+    await userEvent.click(getButton(message));
     expect(fn).toBeCalled();
   });
 });

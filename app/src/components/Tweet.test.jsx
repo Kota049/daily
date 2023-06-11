262d69222f14468f9e5246ba0f4da49ea14fa958
Kota <yuzukh0211@gmail.com>
2023-06-11 21:10:23 +0900

    red has ツイートbutton

--- app/src/components/Tweet.test.jsx
+++ app/src/components/Tweet.test.jsx
@@ -1,3 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import Tweet from "./Tweet";
+import getButton from "../tests/utils/getButton";
 it("suit", () => {
   expect(1).toBe(1);
+});
+describe("tweet button", () => {
+  it("has ツイートbutton", () => {
+    render(<Tweet />);
+    expect(getButton("ツイート")).not.toBeNull();
+  });
 });
